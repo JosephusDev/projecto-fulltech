e1e12d24ed3c2d28594c9090e5d9d57f18897eb4
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2024-12-26 00:41:24 -0300

    Create Clientes.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,15 @@ import {
   Navigate
 } from 'react-router-dom'
 import Projetos from '@/pages/Projetos'
+import Clientes from '@/pages/Clientes'
 
 export default function RoutesComponent() {
   return (
     <Routes>
       {/* Rotas públicas */}
-      <Route path='/projetos' element={<Projetos />} />
       <Route path='/' element={<Projetos />} />
+      <Route path='/projetos' element={<Projetos />} />
+      <Route path='/clientes' element={<Clientes />} />
 
       {/* Rota para lidar com páginas não encontradas */}
       <Route path='*' element={<Navigate to='/' />} />
