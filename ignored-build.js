0e9b31454bb48eedd8233c0a0d98a3643bb7bd8b
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-01-09 17:02:22 -0300

    Update ignored-build.js

--- ignored-build.js
+++ ignored-build.js
@@ -1,8 +1,8 @@
 const url = process.env.VERCEL_URL // URL do deploy
 if (url && url.includes('source=hook')) {
-	console.log('Deploy triggered by hook. Allowing build.')
+	console.log('Deploy triggered by hook. Allowing build ' + url)
 	process.exit(1)
 } else {
-	console.log('Deploy not triggered by hook. Ignoring build.')
+	console.log('Deploy not triggered by hook. Ignoring build ' + url)
 	process.exit(0)
 }
