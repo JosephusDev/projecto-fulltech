ca056d99d9eab03e5ebb9860e187facd1a838e72
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-01-09 18:22:51 -0300

    Update ignored-build.js

--- ignored-build.js
+++ ignored-build.js
@@ -1,9 +1,9 @@
-const triggerSource = process.env.SOURCE || 'undefined'
+const triggerSource = process.env.VERCEL_GIT_COMMIT_REF || 'undefined'
 
-if (triggerSource === 'hook') {
-	console.log('Deploy triggered by a hook. Allowing build: ' + triggerSource)
-	process.exit(1) // Retorna 1 para permitir o build.
-} else {
+if (triggerSource) {
 	console.log('Deploy not triggered by a hook. Ignoring build: ' + triggerSource)
 	process.exit(0) // Retorna 0 para ignorar o build.
+} else {
+	console.log('Deploy triggered by a hook. Allowing build: ' + triggerSource)
+	process.exit(1) // Retorna 1 para permitir o build.
 }
