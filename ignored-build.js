cec91fc98c2001f24e2a922fcb04fe241f31fa1e
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-01-09 17:29:53 -0300

    Delete ignored-build.js

--- ignored-build.js
+++
@@ -1,9 +0,0 @@
-const branch = process.env.VERCEL_GIT_COMMIT_REF || 'unknown'
-
-if (branch === 'main') {
-	console.log('Deploy triggered by branch main. Allowing build.')
-	process.exit(1) // Permite o build
-} else {
-	console.log(`Deploy triggered by branch ${branch}. Ignoring build.`)
-	process.exit(0) // Ignora o build
-}
