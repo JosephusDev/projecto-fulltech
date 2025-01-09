ecb4b90bc8ec6357c20124fe9cb0de96d49731e2
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2025-01-09 19:43:42 -0300

    ababab

--- ignored-build.js
+++ ignored-build.js
@@ -2,9 +2,9 @@
 const commitMessage = process.env.VERCEL_GIT_COMMIT_MESSAGE || ''
 
 if (commitMessage.includes('[deploy-hook]')) {
-	console.log('✅ - Build permitida (commit do deploy hook)')
+	console.log('✅ - Build permitida (commit do deploy hook).')
 	process.exit(1) // Permite o build
 } else {
-	console.log('🚫 - Build ignorada (commit normal)')
+	console.log('🚫 - Build ignorada (commit normal).')
 	process.exit(0) // Ignora o build
 }
